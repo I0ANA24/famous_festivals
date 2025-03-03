4a7316cc8e18ea2d4b969902f61a26e0a3c16aa6
75781040+I0ANA24 <75781040+I0ANA24@users.noreply.github.com>
2025-03-03 19:05:30 +0200

    first page for mobile

---
+++ script.js
@@ -0,0 +1,30 @@
+let hamburger = document.getElementById('menu-icon');
+let close = document.getElementById('menu-icon-close');
+let menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    console.log("merge");
+    hamburger.style.display = "none";
+    close.style.display = "block";
+    menu.style.display = "flex";
+
+    document.addEventListener('click', (event) => {
+        if(close.style.display === "block" && !menu.contains(event.target) && !hamburger.contains(event.target)) {
+            close.style.display = "none";
+            hamburger.style.display = "block";
+            menu.style.display = "none";
+            document.body.style.overflow = "scroll";
+
+            if(event.target.tagName === 'A' || event.target.tagName === 'IMG') {
+                event.preventDefault();
+            }
+        }
+    });
+});
+
+close.addEventListener('click', () => {
+    close.style.display = "none";
+    hamburger.style.display = "block";
+    menu.style.display = "none";
+    document.body.style.overflow = "scroll";
+});
